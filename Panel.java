d0a8d924c64f8979a09704b0ebed06bdad8ea194
birchc1 <c.birch1@lancaster.ac.uk>
2024-05-17 11:02:58 +0100

    random variables

--- Panel.java
+++ Panel.java
@@ -4,11 +4,17 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Panel extends JPanel {
-    private static final int CELL_SIZE = 50;
+    private static final int CELL_SIZE = 30;
     private final Model model;
     public Panel(Model model) {
         this.model = model;
         setPreferredSize(new Dimension(gridSize * CELL_SIZE + 100, gridSize * CELL_SIZE + 100));
         setBackground(Colour.WHITE);
+    }
+    private class MouseListener extends MouseAdapter {
+        public void mouseCLicked(mouseEvent e) {
+            int x = e.getX();
+            int y = e.getY();
+        }
     }
 }  
